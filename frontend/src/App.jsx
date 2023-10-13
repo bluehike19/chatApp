f1b37486929e78dae000d92b789beac18fa1249f
simo <wambuisimon712@gmail.com>
2023-10-13 13:20:38 +0300

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,20 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="gradient_bg">
+    <div className="gradient__bg">
       <h1 className="gradient__text">
         hello Lorem ipsum, dolor sit amet consectetur adipisicing elit. In,
-        ratione.
+        ratione. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Soluta, quae cumque a dolor harum expedita fugit ex, delectus quas qui
+        labore laudantium necessitatibus tempore illo totam iusto blanditiis
+        sequi voluptatum? Iusto rerum ipsam quibusdam praesentium blanditiis
+        quod alias, consectetur aperiam incidunt dicta eum, perspiciatis cum
+        adipisci sit vitae illo exercitationem optio voluptatem in nesciunt hic
+        voluptate molestias ratione voluptatibus. Eaque ipsum inventore quisquam
+        delectus! Voluptate perferendis ab eum ipsa nam. Omnis id, eius eum
+        error quam veniam deserunt minus dolorum, similique culpa, illo nemo!
+        Eligendi at, doloribus cupiditate, magni iste corrupti numquam minima
+        alias illo itaque obcaecati tempore, sunt voluptates?
       </h1>
     </div>
   );

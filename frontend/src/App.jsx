1c227371354fb395bec59ab6543efc0888e04e10
simo <wambuisimon712@gmail.com>
2023-10-13 13:12:10 +0300

    deleted

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="gradient_bg">
+      <h1 className="gradient__text">
+        hello Lorem ipsum, dolor sit amet consectetur adipisicing elit. In,
+        ratione.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
